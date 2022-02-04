16802a67ed64769c18dd700207a7b3ed40dc78ed
Lucie Kana <81702035+LucieKana@users.noreply.github.com>
2022-02-03 20:02:27 -0500

    Add files via upload

---
+++  StudentScore.java
@@ -0,0 +1,21 @@
+public class StudentScore{
+    public static void main(String[] args){
+        int score = 96;
+        if(score >=90){
+            System.out.println("You score A");
+        }
+        else if(score>=80){
+            System.out.println("You score B");
+        }
+        else if(score>=70){
+            System.out.println("You score C");
+        }
+        else if (score>=55){
+            System.out.println("You score D");
+        }
+        else{
+            System.out.println("You score F");
+        }
+        
+    }
+}
