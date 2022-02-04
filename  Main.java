5728da1b838c6a4d4fbcdb68dbc2f073cc932b1a
Lucie Kana <81702035+LucieKana@users.noreply.github.com>
2022-02-03 20:03:00 -0500

    Add files via upload

---
+++  Main.java
@@ -0,0 +1,30 @@
+public class Main{
+    public static void main(String[] args){
+        int day= 6;
+        switch (day){
+            case 1:
+            System.ou.println("Monday");
+            break;
+            case 2:
+            System.ou.println("Tuesday");
+            break;
+            case 3:
+            System.ou.println("Wednesday");
+            break;
+            case 4:
+            System.ou.println("Thursday");
+            break;
+            case 5:
+            System.ou.println("Friday");
+            break;
+            case 6:
+            System.ou.println("Saturday");
+            break;
+            case 7:
+            System.ou.println("Sunday");
+            break;
+            default:
+            System.out.println("invalid input")
+        }
+    }
+}
